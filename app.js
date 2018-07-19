08e9065af788eb92a3164a6ac66ce2f6f9d396f3
Athoof <athoof@gmail.com>
2018-07-19 15:41:22 +0500

    Functional, needs to be applied for every hit on every url

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
     // generateURL(1, 13); //grab urls of pages 1 to 13
 
     // urls.forEach(url => {//will loop through each url, commented out for testing on one url for now
-    request('http://ibay.com.mv/apartments-houses-for-rent-b25_7.html', (error, response, html) => {
+    request('http://ibay.com.mv/apartments-houses-for-rent-b25_3.html', (error, response, html) => {
         if (error)
             throw error;
         var $ = cheerio.load(html);
@@ -37,11 +37,25 @@ app.get('/', (req, res) => {
         console.log($);
 
         let data = $('.iw-list-view-heading-col').first();//only first result per url for testing
-        rooms = data.find('.iw-item-cond').text();//strip html and return string
-        rooms = Number.parseInt(rooms[rooms.search(/\d/)], 10);//search for a digit and convert to int
-        console.log(rooms);
+        
+        let strippedData = data.find('.iw-item-cond').text();//strip html and return string
+        rooms = Number.parseInt(strippedData[strippedData.search(/\d/)], 10);//search for a digit and convert to int
+        
+        let dataToSplit = strippedData.replace(/.*Furnishing :/, '');//Gets furnishing and location line in one string
+        let split = dataToSplit.split(/\WFurnished/);//splits that string at '...Furnished'
+
+        furnishing = split[0].trim();
+        location = split[1].trim();
+
+        rent = $('.iw-grid-p-price').text();
+        rent = rent.replace(/(MVR )/, '');//carrying out these two procedures 
+        rent = rent.replace(/\D/, '');//////simultaneously caused unexpected results
+        rent = Number.parseInt(rent, 10);
+
         json.rooms = rooms;
-        json.rent = 'test';
+        json.furnishing = furnishing;
+        json.location = location;
+        json.rent = rent;
 
         if (response)
             res.send(json)
